fe5f4b40cb29ef44cac2c76305b1509b31e845cd
Shayan <shayan3k@gmail>
2020-06-26 12:57:12 +0430

    Updated

--- client/src/componenets/pages/public/SignIn/index.js
+++ client/src/componenets/pages/public/SignIn/index.js
@@ -1,57 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import "./SignIn.scss";
-import Logo from "../../../partials/public/assets/images/logo.png";
-import { Col, Row, Button, Form, FormGroup, Label, Input } from "reactstrap";
-import Alert from "../../../partials/public/components/Alert";
-import SignUp from "../../../partials/public/components/Signup";
-import Longin from "../../../partials/public/components/Login";
 
-const Account = ({ props }) => {
-  const [tabState, setTabState] = useState(false);
+import Signin from "../../../partials/public/containers/Signin";
 
-  return (
-    <>
-      <section className="login-wrapper">
-        <div className="tab_container">
-          <div className="logo-box">
-            <Link to="/">
-              <img src={Logo} alt="Logo" />
-            </Link>
-          </div>
-          <Alert />
-
-          <Input
-            id="tab1"
-            type="radio"
-            name="tabs"
-            checked={tabState ? false : true}
-            onClick={() => {
-              setTabState(false);
-            }}
-          />
-          <label htmlFor="tab1">
-            <span>ورود به حساب کاربری</span>
-          </label>
-
-          <Input
-            id="tab2"
-            type="radio"
-            name="tabs"
-            checked={tabState ? true : false}
-            onClick={() => {
-              setTabState(true);
-            }}
-          />
-          <label htmlFor="tab2">
-            <span>ثبت نام</span>
-          </label>
-
-          <Longin />
-          <SignUp />
-        </div>
-      </section>
-    </>
-  );
-};
-export default Account;
+export default function Dashboard(props) {
+  return <Signin />;
+}
